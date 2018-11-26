4dc63df9f432d8e7094d3be4a7f76ae142312513
Charlie <channer@ssl.umd.edu>
2018-11-26 16:16:53 -0500

    Completed initial run through of hardcoded maze

--- final_project/hardpath.py
+++ final_project/hardpath.py
@@ -13,16 +13,101 @@ pig=easy.EasyGoPiGo3
 d_sensor = pig.init_distance_sensor()
 servo = pig.init_servo() #Check this init
 
+left=True
+
 if left==True:
     #If the first move is to turn left we are in the position S1:
     
 #Orient yourself - somehow. Assuming starting after the turn left facing the first opening
+    #Drive forward 10 inches
     pig.drive_inches(10,blocking=True)
+    #Turn left 90 degrees
     pig.orbit(-90,0,blocking=True)
+    #Drive forward 8 inches
     pig.drive_inches(8,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 10 inches
+    pig.drive_inches(10,blocking=True)
+    #Turn right 90 degrees
     pig.orbit(90,0,blocking=True)
-    
-
-
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #TUrn right 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn right 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(12,blocking=True)
+    #We out the fucking maze bitches
 
 else:
+    
+    #Drive forward 10 inches
+    pig.drive_inches(10,blocking=True)
+    #Turn right 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 10 inches
+    pig.drive_inches(10,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 25 inches
+    pig.drive_inches(25,blocking=True)
+    #TUrn left 90 degrees
+    pig.orbit(-90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn right 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn left 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(8,blocking=True)
+    #Turn right 90 degrees
+    pig.orbit(90,0,blocking=True)
+    #Drive forward 8 inches
+    pig.drive_inches(12,blocking=True)
+    #We out the fucking maze bitches
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
