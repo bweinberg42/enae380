396b74a8f9a8a9ffd58541471110a27785cf7ee1
Blaire Weinberg <bweinberg42@gmail.com>
2018-11-26 12:34:29 -0500

    LED script

---
+++ final_project/lights.py
@@ -0,0 +1,32 @@
+"""
+Script to handle LED for bot
+Can blink and wink
+"""
+
+import time
+import easygopigo3 as easy
+
+bot = easy.EasyGoPiGo3()
+
+
+def blink(color):
+	bot.set_eye_color(color)
+	bot.open_eyes()
+	time.sleep(0.5)
+	bot.close_eyes()
+
+
+def wink_left(color):
+	bot.set_eye_color(color)
+	bot.open_left_eye()
+	time.sleep(0.5)
+	bot.close_left_eye()
+
+
+def wink_right(color):
+	bot.set_eye_color(color)
+	bot.open_right_eye()
+	time.sleep(0.5)
+	bot.close_right_eye()
+
+
