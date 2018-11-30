d2993be3d29deeb32824eabeb84c72d9fd9b3b64
Blaire Weinberg <bweinberg42@gmail.com>
2018-11-30 11:39:07 -0500

    headless camera

--- final_project/camera.py
+++ final_project/camera.py
@@ -3,7 +3,6 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
 sleep(5)
-camera.capture('/home/pi/Desktop/enae380/final_project/image.jpg')
-camera.stop_preview()
+camera.capture('/home/pi/Desktop/enae380/final_project/image.png', format='png')
+camera.close()
