f8c646600743a54452343281534e6a0123d4d4eb
Blaire Weinberg <bweinberg42@gmail.com>
2018-11-30 20:28:02 +0000

    image

--- final_project/camera.py
+++ final_project/camera.py
@@ -3,6 +3,6 @@ from time import sleep
 
 camera = PiCamera()
 
-sleep(5)
-camera.capture('/home/pi/Desktop/enae380/final_project/image.png', format='png')
+sleep(6)
+camera.capture('image.png')
 camera.close()
