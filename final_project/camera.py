ac21a9c907e36161a354f2a465ce2f53dd518501
Blaire Weinberg <bweinberg42@gmail.com>
2018-12-01 19:34:35 +0000

    somewhat decent parameters for camera circle finding

--- final_project/camera.py
+++ final_project/camera.py
@@ -15,17 +15,16 @@ def check_cam():
 img = cv2.imread('image.png',0)
 img = cv2.medianBlur(img,5)
 
-circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 10) #, param1=50, param2=30, minRadius=0, maxRadius=0)
+circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 200, param1=50, param2=30, minRadius=0, maxRadius=0)
 
-print(circles)
-"""
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
 	# draw the outer circle
 	cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
 	# draw the center of the circle
 	cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
-cv2.imshow('detected circles',cimg)
+
+print(circles)
+cv2.imshow('detected circles', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
