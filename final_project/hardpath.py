a4aa24b2d09ac577e9bd05ba0c76a7114cda515b
Blaire Weinberg <bweinberg42@gmail.com>
2018-12-02 23:03:12 +0000

    readjusted more angles and shit

--- final_project/hardpath.py
+++ final_project/hardpath.py
@@ -12,7 +12,7 @@ pig = easy.EasyGoPiGo3()
 d_sensor = pig.init_distance_sensor()
 
 def main():
-	orient.orient_long()
+	#orient.orient_long()
 	pig.drive_inches(-12,blocking=True)
 	left = orient.orient_path()
 	maize(left)
@@ -22,14 +22,15 @@ def maize(left):
 	if left:
 		drive(-13, -90)
 		drive(-12, 90)
-		drive(-12, -80)
+		drive(-12, 80)
 		drive(-13, -80)
 		drive(-11, 90)
 		drive(-11, -90)
 		drive(-20, 0)
     	
 	else:
-		drive(-10, -93)
+		drive(-24, -93)
+		drive(-12, -90)
 		drive(-36, -90)
 		drive(-12, 90)
 		drive(-12, -90)
